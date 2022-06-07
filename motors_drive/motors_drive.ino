628ca5ec83436bf5e8a53e943e249fb6e4c10d9d
Amer Alhamvi <amerhamwi3@gmail.com>
2022-06-07 14:09:35 +0300

    Added combined file, IMU works.

--- motors_drive/motors_drive.ino
+++ motors_drive/motors_drive.ino
@@ -1,5 +1,5 @@
 #include <AFMotor.h>
-char input
+char input;
 
 AF_DCMotor motorL1(1, MOTOR12_64KHZ); // create motor #1, 64KHz pwm
 AF_DCMotor motorL2(2, MOTOR12_64KHZ); // create motor #2, 64KHz pwm
@@ -10,37 +10,43 @@ void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Motor test!");
   
-  motor.setSpeed(255);     // set the speed to 200/255
+  motorL1.setSpeed(255);     // set the speed to 200/255
+  motorL2.setSpeed(255);     // set the speed to 200/255
+  motorR1.setSpeed(255);     // set the speed to 200/255
+  motorR2.setSpeed(255);     // set the speed to 200/255
 }
 
 void loop() {
+  Serial.println("there");
   if(Serial.available()){
     input = Serial.read();
-
+    Serial.println("here");
+  
+  Serial.println(input);
     switch (input){
       
-      case "w":
+      case 'w':
       motorL1.run(FORWARD);
       motorL2.run(FORWARD);
       motorR1.run(FORWARD);
       motorR2.run(FORWARD);
       break;
       
-      case "s":
+      case 's':
       motorL1.run(BACKWARD);
       motorL2.run(BACKWARD);
       motorR1.run(BACKWARD);
       motorR2.run(BACKWARD);
       break;
       
-      case "a":
+      case 'a':
       motorL1.run(FORWARD);
       motorL2.run(FORWARD);
       motorR1.run(BACKWARD);
       motorR2.run(BACKWARD);
       break;
       
-      case "d":
+      case 'd':
       motorL1.run(BACKWARD);
       motorL2.run(BACKWARD);
       motorR1.run(FORWARD);
@@ -48,12 +54,23 @@ void loop() {
       break;
 
       default:
-      motorL1.run(RELEASE);
+      Serial.println("at defult now");
+      motorL1.run(FORWARD);
+      motorL2.run(FORWARD);
+      motorR1.run(FORWARD);
+      motorR2.run(FORWARD);
+      /*motorL1.run(RELEASE);
       motorL2.run(RELEASE);
       motorR1.run(RELEASE);
-      motorR2.run(RELEASE);
+      motorR2.run(RELEASE);*/
+      
       break;
       
     }
+  } else {
+      motorL1.run(FORWARD);
+      motorL2.run(FORWARD);
+      motorR1.run(FORWARD);
+      motorR2.run(FORWARD);
   }
 }
